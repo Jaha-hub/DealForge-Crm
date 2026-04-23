e3c712d43e5b3cdd3cad2032a564073906a8bda5
Jaha-hub <nuratdinovzahangir4@gmail.com>
2026-04-23 17:35:06 +0500

    finish funnel router

--- backend/src/backend/presentation/api/v1/funnel/router.py
+++ backend/src/backend/presentation/api/v1/funnel/router.py
@@ -3,7 +3,13 @@ from fastapi_utils.cbv import cbv
 from starlette import status
 
 from src.backend.application.funnel.dtos.create_funnel import CreateFunnelResult, CreateFunnelCommand
+from src.backend.application.funnel.dtos.list_funnel import ListFunnelCommand
+from src.backend.application.funnel.dtos.update_funnel import UpdateFunnelCommand
 from src.backend.application.funnel.use_cases.create_funnel import CreateFunnelUseCase
+from src.backend.application.funnel.use_cases.delete_funnel import DeleteFunnelUseCase
+from src.backend.application.funnel.use_cases.list_funnel import ListFunnelUseCase
+from src.backend.application.funnel.use_cases.update_funnel import UpdateFunnelUseCase
+from src.backend.application.shared.dtos.pagination import PageResult
 from src.backend.application.shared.interfaces.uow import UnitOfWork
 from src.backend.domain.funnel.entity import Funnel
 from src.backend.domain.user.entity import User
@@ -46,3 +52,47 @@ class FunnelRouter:
             funnel: Funnel = Depends(get_funnel)
     ):
         return funnel
+
+    @router.put(
+        "/funnel_update/{funnel_id}",
+        response_model=Funnel
+    )
+    async def update_funnel(
+            self,
+            request: UpdateFunnelCommand,
+            funnel: Funnel = Depends(get_funnel),
+    ):
+        uc = UpdateFunnelUseCase(
+            uow=self.uow,
+            user=self.user,
+            funnel=funnel,
+        )
+        response = await uc.execute(request)
+        return response
+
+    async def delete_funnel(
+            self,
+            funnel: Funnel = Depends(get_funnel),
+    ):
+        uc = DeleteFunnelUseCase(
+            uow=self.uow,
+            user=self.user,
+            funnel=funnel,
+        )
+        await uc.execute()
+        return None
+
+    @router.get(
+        "/funnels",
+        response_model=PageResult[Funnel]
+    )
+    async def list_funnels(
+            self,
+            command: ListFunnelCommand = Depends(),
+    ):
+        uc = ListFunnelUseCase(
+            uow=self.uow,
+            user=self.user,
+        )
+        result = await uc.execute(command)
+        return result
